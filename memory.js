76cc2c4521baa603f66d04e0fa237378be0fc0b9
Test <justtest@gmail.com>
2020-04-14 18:50:30 +0200

    first commit

---
+++ memory.js
@@ -0,0 +1,3 @@
+const cards = ["coffee-heart.jpg", "coffee-heart.jpg", "coffeehold.jpg", "coffeehold.jpg", 
+"kawa-1.jpg", "kawa-1.jpg", "kawa2.jpg", "kawa2.jpg", "kawa-5526.jpg", "kawa-5526.jpg",
+ "KawaAZ.jpg", "KawaAZ.jpg"]
